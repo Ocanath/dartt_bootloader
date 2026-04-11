c6ac806a740e9ef8b57d2249e84ff8c99f23a577
ocanath <ocanath@gmail.com>
2026-04-11 15:13:37 -0700

    clean

---
+++ bootloaders/STM32G431CBTx/Core/Inc/init.h
@@ -0,0 +1,26 @@
+/*
+ * init.h
+ *
+ *  Created on: Apr 11, 2026
+ *      Author: redux
+ */
+
+#ifndef INC_INIT_H_
+#define INC_INIT_H_
+#include "main.h"
+#include "m_dma_uart.h"
+
+extern FDCAN_HandleTypeDef hfdcan1;
+
+extern UART_HandleTypeDef huart2;
+extern DMA_HandleTypeDef hdma_usart2_rx;
+extern DMA_HandleTypeDef hdma_usart2_tx;
+
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_DMA_Init(void);
+void MX_FDCAN1_Init(void);
+void MX_USART2_UART_Init(void);
+
+
+#endif /* INC_INIT_H_ */
