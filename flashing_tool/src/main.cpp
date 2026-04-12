a8014151f781c81e6630c3d536e833d031c64b0e
ocanath <ocanath@gmail.com>
2026-04-12 15:46:05 -0700

    basic wiring

--- flashing_tool/src/main.cpp
+++ flashing_tool/src/main.cpp
@@ -12,4 +12,13 @@ int main(int argc, char** argv)
 {
 	args_t args = parse_args(argc, argv);
 	DarttFlasher flasher(args.dartt_address);
+	flasher.ser.autoconnect(921600);
+	flasher.bootloader_control.action_flag = START_APPLICATION;
+	dartt_buffer_t ctl = 
+	{
+		.buf = (unsigned char *)(&flasher.bootloader_control.action_flag), 
+		.size = sizeof(uint32_t), 
+		.len = sizeof(uint32_t)
+	};
+	dartt_sync(&ctl, &flasher.ds);
 }
